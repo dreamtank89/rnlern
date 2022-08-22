17df756c01f1b8edba111b024fd52066e566840e
Farhan <farhanmac@Farhans-MacBook-Air.local>
2022-08-22 23:59:28 +0800

    first react native commit

---
+++ screen/HomeScreen.js
@@ -0,0 +1,20 @@
+import { Text, SafeAreaView } from 'react-native';
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from "@react-navigation/native"
+
+function HomeScreen() {
+    const navigation = useNavigation()
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerShown: false
+        })
+    },[])
+    return(
+        <SafeAreaView>
+            <Text className="text-red-500">This is HomeScreen</Text>            
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
---
+++ screen/ProfileScreen.js
@@ -0,0 +1,12 @@
+import { SafeAreaView , Text} from "react-native";
+
+function ProfileScreen() {
+
+    return(
+        <SafeAreaView>
+            <Text>This is Profile Screen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default ProfileScreen
